ceac4f94bcdb894e3e236fd9fb7205206da8824d
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-13 20:35:15 +0200

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello from brand new AWS CodeStar project!',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
